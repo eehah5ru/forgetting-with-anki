a11bd628b13ad3b76289a7ba1586a113744a9623
Nicolay Spesivtsev <nicola.spesivcev@gmail.com>
2022-05-24 13:41:54 +0300

    [deploy] updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ USER=pi
 HOST=pi4.local
 
 rsync -avz * $USER@$HOST:/home/pi/.local/share/Anki2/addons21/forgetting/
-rsync -avz * $USER@$HOST:/home/pi/.anki/Anki2/addons21/forgetting/
+rsync -avz * $USER@$HOST:/home/pi/.anki/en/Anki2/addons21/forgetting/
+rsync -avz * $USER@$HOST:/home/pi/.anki/ru/Anki2/addons21/forgetting/
 
 ssh $USER@$HOST "sudo systemctl restart anki.service"
